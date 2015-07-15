5a6e2dba5b719ae4354feb6b3efb95b25ec1a2a8
Victor Kane <victorkane@gmail.com>
2015-07-15 20:39:02 -0300

    Data provided to all controllers that invoke it as an injected service known throughout the app

--- public/js/recipejs-app.js
+++ public/js/recipejs-app.js
@@ -1,17 +1,32 @@
-angular.module('recipeJSApp', [])
-.controller('ControllerHeader', [function() {
-  var vm = this;
-  vm.message = 'Welcome';
-  vm.titletext = 'RecipeJS';
-  vm.edition = 'First';
-}])
-.controller('ControllerRecipe', [function() {
-  var vm = this;
-  vm.recipes = [
+function RecipeService() {
+  var recipes = [
     {title: "Baked Beans on Toast", new: true},
     {title: "French Toast", new: false},
     {title: "Rocky Mountain Egg", new: false},
   ];
+  this.list = function() {
+    return recipes;
+  };
+  this.add = function(recipe) {
+    recipes.push(recipe);
+  };
+}
+
+angular.module('recipeJSApp', [])
+.controller('ControllerHeader', ['RecipeService', function(RecipeService) {
+  var vm = this;
+  vm.message = 'Welcome';
+  vm.titletext = 'RecipeJS';
+  vm.edition = 'First';
+  vm.list = function() {
+    return RecipeService.list();
+  };
+}])
+.controller('ControllerRecipe', ['RecipeService', function(RecipeService) {
+  var vm = this;
+  vm.list = function() {
+    return RecipeService.list();
+  };
 }])
 .controller('ControllerUser', [function() {
 var vm = this;
@@ -19,4 +34,5 @@ vm.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
 vm.submit = function() {
   console.log('User signed in: ', vm.user)
 };
-}]);
+}])
+.service('RecipeService', [RecipeService]);
