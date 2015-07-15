15f26c91b2a37beaa856b53184f2f50f8fc1b059
Victor Kane <victorkane@gmail.com>
2015-07-15 10:17:43 -0300

    master now compatible with cloud 9 ide

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var serveStatic = require('serve-static')
 
 var app = express()
 app.use(morgan('combined'))
-var port = 3000;
 app.use(serveStatic(__dirname + '/public'))
 
+var url = process.env.IP || '0.0.0.0'
+var port = 3000;
 app.set('port', process.env.PORT || port)
 
-var server = app.listen(app.get('port'), function () {
-  console.log('Static server listening on port %s', server.address().port);
+var server = app.listen(app.get('port'), url, function () {
+  console.log('Static server listening url %s on port %s', url, server.address().port);
 })
