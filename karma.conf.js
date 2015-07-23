6283c9c6f2e5d152fdfc2778468a497db4ac4135
Victor Kane <victorkane@gmail.com>
2015-07-23 16:11:30 -0300

    reorganized location for unit and e2e tests so karma can run unit tests with jasmine while protractor can also run

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       './public/**/js/*.js',
-      './test/**/*.js'
+      './test/unit/**/*.js'
     ],
 
 
