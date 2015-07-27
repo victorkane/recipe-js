ee5963642b7919f4e24807a161826a98d7467496
Victor Kane <victorkane@gmail.com>
2015-07-27 13:26:39 -0300

    source files in src directory, distribution files in untracked public directory

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,46 @@ var gulp = require('gulp'),
 var CacheBuster = require('gulp-cachebust');
 var cachebust = new CacheBuster();
 
-gulp.task('default', function() {
-  console.log('Hello, world!');
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// cleans the build output
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('clean', function (cb) {
+    del([
+        'dist'
+    ], cb);
+});
+
+gulp.task('build', ['clean'], function() {
+  gulp.src(['src/**/*.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('public/js'))
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Build a minified Javascript bundle - the order of the js files is determined
+// by browserify
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('build-js', ['clean'], function() {
+    var b = browserify({
+        entries: './src/js/recipejs-app.js',
+        debug: true,
+        paths: ['./js/controllers', './js/services', './js/directives'],
+        transform: [ngAnnotate]
+    });
+
+    return b.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(cachebust.resources())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public/js/'));
 });
