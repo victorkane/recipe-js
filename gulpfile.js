df12993f4f1396b2669e990578edd7179191250f
Victor Kane <victorkane@gmail.com>
2015-07-27 15:16:55 -0300

    basic build going, bundle not referenced yet - broken

--- gulpfile.js
+++ gulpfile.js
@@ -29,11 +29,46 @@ gulp.task('clean', function (cb) {
     ], cb);
 });
 
-gulp.task('build', ['clean'], function() {
-  gulp.src(['src/**/*.js'])
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('public/js'))
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// runs bower to install frontend dependencies
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('bower', function() {
+
+    var install = require("gulp-install");
+
+    return gulp.src(['./bower.json'])
+        .pipe(install());
 });
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// runs sass, creates css source maps
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('build-css', ['clean'], function() {
+    return gulp.src('./src/styles/*')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(cachebust.resources())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./public/css'));
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// runs jshint
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('jshint', function() {
+    gulp.src('./src/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+})
 
 /////////////////////////////////////////////////////////////////////////////////////
 //
@@ -59,4 +94,51 @@ gulp.task('build-js', ['clean'], function() {
         .on('error', gutil.log)
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/js/'));
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Generates a sprite png and the corresponding sass sprite map.
+// This is not included in the recurring development build and needs to be run separately
+// Task copyimages should be removed from task dev and build
+// if this task is to be used.
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('sprite', function () {
+
+    var spriteData = gulp.src('./src/images/*.png')
+        .pipe(spritesmith({
+            imgName: 'todo-sprite.png',
+            cssName: '_todo-sprite.scss',
+            algorithm: 'top-down',
+            padding: 5
+        }));
+
+    spriteData.css.pipe(gulp.dest('./dist'));
+    spriteData.img.pipe(gulp.dest('./dist'))
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// simply copies images
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('copyimages', function() {
+  gulp.src('./src/images/**/*')
+  .pipe(gulp.dest('./public/images'))
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// full build (except sprites), applies cache busting to the main page css and js bundles
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+//gulp.task('build', [ 'clean', 'bower','build-css','build-template-cache', 'jshint', 'build-js'], function() {
+gulp.task('build', [ 'clean', 'bower','build-css',/* 'build-template-cache',*/ 'jshint', 'build-js'], function() {
+    return gulp.src('./src/index.html')
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('public'));
 });
