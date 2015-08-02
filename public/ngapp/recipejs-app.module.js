a8f5c7537ffd248b486d3e53249c7008927a28ea
Victor Kane <victorkane@gmail.com>
2015-08-02 14:06:16 -0300

    Initial ui-router based application state configuration and templating; broken unless public/vendor contains dependencies; never fear gulp coming soon

--- public/ngapp/recipejs-app.module.js
+++ public/ngapp/recipejs-app.module.js
@@ -1,5 +1,21 @@
-angular.module('recipeJSApp', [
-    'header',
-    'recipes',
-    'users'
-]);
+angular.module('RecipeJSApp', [
+        'ui.router',
+        'header',
+        'recipes',
+        'users'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home', {
+                url: '/', // Make to navigate to index.html#/
+                templateUrl: 'ngapp/header/header.tmpl.html',
+                controller: 'RecipeJSCtrl'
+            })
+            ;
+    })
+    .controller('RecipeJSCtrl', [function() {
+        var recipeJSCtrl = this;
+    }]);
+//    .controller('RecipeJSCtrl', [function() {
+//    }]);
