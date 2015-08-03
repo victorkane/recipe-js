08ac5d14f8ce605a2d7b7fdb0967bb72b131532c
Victor Kane <victorkane@gmail.com>
2015-08-03 13:42:30 -0300

    gulp copy-img-dev implemented

--- gulpfile.js
+++ gulpfile.js
@@ -16,18 +16,23 @@ var cachebust = new CacheBuster();
 /*
  * cleaning tasks
  */
- gulp.task('clean-dev', function (cb) {
-     del(['./public'], cb);
- });
+gulp.task('clean-dev', function (cb) {
+    del(['./public'], cb);
+});
 
- gulp.task('clean-prod', function (cb) {
+gulp.task('clean-prod', function (cb) {
     del(['./dist'], cb);
 });
 
 /*
  * copying tasks
  */
- gulp.task('copy-bower-components-dev', function() {
-   gulp.src('./bower_components/**/*.min.*')
-   .pipe(gulp.dest('./public/vendor'))
- });
+gulp.task('copy-bower-components-dev', function () {
+    gulp.src('./bower_components/**/*.min.*')
+        .pipe(gulp.dest('./public/vendor'))
+});
+
+gulp.task('copy-img-dev', function () {
+    gulp.src('./src/images')
+    .pipe(gulp.dest('./public'))
+});
