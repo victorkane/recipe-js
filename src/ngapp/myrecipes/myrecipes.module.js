6f155e85dc1175e151e3fdb205005e58ed8599d4
Victor Kane <victorkane@gmail.com>
2015-08-06 17:36:58 -0300

    Make filter programmatic and therefore reusable

--- src/ngapp/myrecipes/myrecipes.module.js
+++ src/ngapp/myrecipes/myrecipes.module.js
@@ -1,14 +1,24 @@
 angular.module('myrecipes', [
         'shared.recipes.service'
     ])
-    .controller('MyRecipesCtrl', ['RecipeService', 'UserService', function (RecipeService, UserService) {
+    .controller('MyRecipesCtrl', ['RecipeService', 'UserService', '$filter', function (RecipeService, UserService, $filter) {
         var myRecipesCtrl = this;
         myRecipesCtrl.cuser = '';
+        myRecipesCtrl.myRecipes = [];
         myRecipesCtrl.list = function () {
-             return RecipeService.listRecipes();
+            return RecipeService.listRecipes();
+        };
+        myRecipesCtrl.listMyRecipes = function () {
+            return RecipeService.listRecipes();
         };
         myRecipesCtrl.currentUserID = function () {
             myRecipesCtrl.cuser = UserService.getCurrentUser();
+            if (myRecipesCtrl.cuser.id) {
+                var theRecipes = RecipeService.listRecipes();
+                myRecipesCtrl.myRecipes = $filter('filter')(theRecipes, {
+                    userid: myRecipesCtrl.cuser.id
+                });
+            }
             return myRecipesCtrl.cuser.id;
         };
     }]);
